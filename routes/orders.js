cc79a4c726dda94c72d1395fde86572a606e3be4
Julian Tran <juliantran003@gmail.com>
2021-07-15 21:16:01 +0100

    user orders route

--- routes/orders.js
+++ routes/orders.js
@@ -13,4 +13,22 @@ router.get(`/orders`, async (req, res) => {
   }
 });
 
+router.get(`/orders/:user`, async (req, res) => {
+  console.log("Using Route : /orders/:user");
+  try {
+    const orders = await Order.find();
+    const newOrders = [];
+    if (orders.findIndex((x) => x.customer.userId === req.params.user) !== -1) {
+      newOrders.push(
+        orders[
+          orders.findIndex((x) => x.customer.userId === req.params.user) !== -1
+        ]
+      );
+      res.status(200).json(newOrders);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
