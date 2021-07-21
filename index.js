4b62778fd3625fbbb4d9bc516d555775baf660b6
Julian Tran <juliantran003@gmail.com>
2021-07-21 20:14:25 +0100

    changed cors auth

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 
   next();
 });
