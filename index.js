7150e6694cd632121b4b10fff0f1bc0961b596f3
Julian Tran <juliantran003@gmail.com>
2021-07-21 19:17:46 +0100

    removed 3001

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE, OPTIONS");
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
 
   next();
 });
