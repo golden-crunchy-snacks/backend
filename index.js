ca45629b36f3ef4c292c5facc22d5e4aa2c2d67c
Julian Tran <juliantran003@gmail.com>
2021-07-21 12:19:23 +0100

    resolving cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE, OPTIONS");
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 
   next();
